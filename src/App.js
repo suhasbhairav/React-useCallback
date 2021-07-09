51dc2e314fe7a94a2050ae64398b17f7746ccadf
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-09 17:07:05 +0200

    Initial changes
    Initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useMemo, useCallback, useEffect} from 'react';
 
 function App() {
+
+  const [counter, setCounter] = useState(1);
+  const result = useMemo(() => {
+    return factorial(counter);
+  }, [counter]);
+  
+  const [name, setName] = useState("");
+
+ 
+
+  const displayName = useCallback(
+    () => {
+      return name;
+    },
+    [name],
+  )
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          Factorial of {counter} is {result}
+        </div>
+        <button onClick={() => setCounter(counter + 1)}>Increase</button>
+        <button onClick={() => setCounter(counter - 1)}>Decrease</button>
+        <div>
+          <input type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)} />
+          Name: {name}
+        </div>
+        <DisplayName displayName={displayName}></DisplayName>
+        
     </div>
   );
-}
+};
 
+const DisplayName = ({displayName}) => {
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    setValue(displayName())
+  }, [displayName]);
+  return <div>
+      {value}
+  </div>
+};
+
+const factorial = (num) => {
+  if(num < 0){
+    return -1;
+  }
+
+  if (num === 0){
+    return 1;
+  }
+
+  return num * factorial(num-1);
+}
 export default App;
